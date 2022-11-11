f6865164a65c93855a64d76aaf53e33b39412ed4
CodeSoftly <eclittlejohn@yahoo.com>
2022-11-10 23:31:12 -0600

    added comments

--- functions.php
+++ functions.php
@@ -19,11 +19,13 @@
     add_action('after_setup_theme', 'university_features');
 
     function university_adjust_queries($query) {
+        // Order archive posts that = program
         if(!is_admin() && is_post_type_archive('program') && is_main_query()) {
             $query->set('orderby', 'title');
             $query->set('order', 'ASC');
             $query->set('posts_per_page', -1);
         }
+        // Order archive posts that = event
         if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
             $today = date('Ymd');
             $query->set('meta_key', 'event_date');
@@ -40,6 +42,7 @@
         }
     }
 
+    // Call action hook to adjust queries
     add_action('pre_get_posts', 'university_adjust_queries');
 
 ?>
